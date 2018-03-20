6eefd90d3fe31d3812f0225827b0e4f2dd020757
kjisoo <kim@jisoo.net>
2018-03-20 16:19:29 +0900

    Add testcase for FooService created with Quick and Nimble

--- testTests/FooServiceSpec.swift
+++ testTests/FooServiceSpec.swift
@@ -2,34 +2,73 @@
 //  FooServiceSpec.swift
 //  testTests
 //
-//  Created by sonny on 2018. 3. 20..
+//  Created by jisoo on 2018. 3. 20..
 //  Copyright © 2018년 jisoo. All rights reserved.
 //
 
-import XCTest
+import Quick
+import Nimble
+@testable import test
 
-class FooServiceSpec: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+class FooServiceSpec: QuickSpec {
+  override func spec() {
+    describe("FooService") {
+      var fooService: FooService!
+      beforeEach {
+        fooService = FooService()
+      }
+
+      context("Pass parameters") {
+        it("Compares parameters equally using waitUntil.") {
+          let text = "Test param string"
+          waitUntil(timeout: 2) { done in
+            fooService.execute(param: text, delay: 1, complete: { (param) in
+              expect((param as! String)).to(equal(text))
+              done()
+            })
+          }
         }
+        
+        it("Compares parameters equally using toEventually.") {
+          let text = "Test param string"
+          var inBlockText: String = ""
+          fooService.execute(param: text, delay: 1, complete: { (param) in
+            inBlockText = param as! String
+            
+          })
+          expect(inBlockText).toEventually(equal(text), timeout: 2)
+        }
+      }
+      
+      describe("About complete block") {
+        it("Execute complete block") {
+          waitUntil(timeout: 2) { done in
+            fooService.execute(param: "", delay: 1, complete: { (param) in
+              done()
+            })
+          }
+        }
+
+        context("Cancel execute complete block") {
+          it("When call cancel method") {
+            fooService.execute(param: "", delay: 1, complete: { (param) in
+              fail()
+            })
+            fooService.cancel()
+          }
+          
+          it("When call execute again") {
+            waitUntil(timeout: 2) { done in
+              fooService.execute(param: "", delay: 0.5, complete: { (param) in
+                fail()
+              })
+              fooService.execute(param: "", delay: 1, complete: { (param) in
+                done()
+              })
+            }
+          }
+        }
+      }
     }
-    
+  }
 }
